4b5f58b38cd5978b150560185ec2478f9fbc5699
23030159-dotcom <23030159@student.bdu.edu.vn>
2026-07-02 09:38:32 +0700

    Update app.py

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ import time
 # ---------------------------------------------------------------------------
 st.set_page_config(
     page_title="Hệ Thống Thẩm Định Tín Dụng Doanh Nghiệp
-    - Đề tài 7",
+- Đề tài 7",
     page_icon="🏢",
     layout="wide",
     initial_sidebar_state="expanded",
